9c55eb5a678846e204d974b18ae3924eeca6e2e8
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2021-10-23 23:07:18 +0000

    Position circle fixed to the body of the page

--- rotating-navigation/script.js
+++ rotating-navigation/script.js
@@ -1,15 +1,15 @@
 const container = document.querySelector('.container');
 const close = document.querySelector('.close');
 const open = document.querySelector('.open');
-// const circle = document.querySelector('.circle');
+const circle = document.querySelector('.circle');
 
 
 close.addEventListener('click', () => {
     container.classList.remove('show-nav')
-    // circle.classList.remove('rotate')
+    circle.classList.remove('rotate')
 })
 
 open.addEventListener('click', () => {
     container.classList.add('show-nav')
-    // circle.classList.add('rotate')
+    circle.classList.add('rotate')
 })
